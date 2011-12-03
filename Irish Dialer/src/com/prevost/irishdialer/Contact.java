470450c9f151e7fff1692d04878e44d02b254f25
Clement PREVOST <prevostclement@gmail.com>
2011-12-03 19:07:56 +0100

    refactoring contact data pull methods

--- Irish Dialer/src/com/prevost/irishdialer/Contact.java
+++ Irish Dialer/src/com/prevost/irishdialer/Contact.java
@@ -2,6 +2,13 @@ package com.prevost.irishdialer;
 
 import java.util.ArrayList;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.preference.PreferenceManager;
+import android.provider.ContactsContract;
+
 public class Contact {
 
 	protected ArrayList<String> _infos = null; 
@@ -34,4 +41,74 @@ public class Contact {
 		}
 		return false;
 	}
+
+	public static ArrayList<String> getInfosFromCursorAndPreferences(ContentResolver cr, Context context, Cursor cur, String fullName) {
+    	ArrayList<String> infos = new ArrayList<String>();
+    	SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        
+    	String contactId = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+    	
+    	if (settings.getBoolean("full_name", true)) {
+        	if (null != fullName) {
+        		infos.add(fullName);
+        	} 
+        }
+    	
+    	if (settings.getBoolean("email", true)) {
+    		infos.addAll(_getContactData(cr, contactId, 
+    				ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE, 
+    				ContactsContract.CommonDataKinds.Email.DATA));
+    	}
+    	
+    	if (settings.getBoolean("phone", true)) {
+    		infos.addAll(_getContactData(cr, contactId, 
+    				ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE, 
+    				ContactsContract.CommonDataKinds.Phone.NUMBER));
+    	}
+    	
+    	if (settings.getBoolean("nickname", true)) {
+    		infos.addAll(_getContactData(cr, contactId, 
+    				ContactsContract.CommonDataKinds.Nickname.CONTENT_ITEM_TYPE, 
+    				ContactsContract.CommonDataKinds.Nickname.NAME));
+    	}
+    	
+    	if (settings.getBoolean("url", true)) {
+ 	       infos.addAll(_getContactData(cr, contactId, 
+ 	    		   ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE, 
+ 	    		   ContactsContract.CommonDataKinds.Website.URL));
+    	}
+    	
+    	if (settings.getBoolean("instant_message", true)) {
+    		infos.addAll(_getContactData(cr, contactId, 
+    				ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE, 
+    				ContactsContract.CommonDataKinds.Im.DATA));
+    	}
+    	
+    	if (settings.getBoolean("note", true)) {
+    		infos.addAll(_getContactData(cr, contactId, 
+    				ContactsContract.CommonDataKinds.Note.CONTENT_ITEM_TYPE, 
+    				ContactsContract.CommonDataKinds.Note.NOTE));
+    	}
+    	
+    	
+        return infos;
+    }
+    
+
+	protected static ArrayList<String> _getContactData(ContentResolver cr, String contactId, String contentItemType, String dataField) {
+		ArrayList<String> infos = new ArrayList<String>();
+		Cursor cur = cr.query(
+			ContactsContract.Data.CONTENT_URI , null, 
+			ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?", 
+ 		    new String[]{contactId, contentItemType}, null);
+        while (cur.moveToNext()) {
+        	  String info = cur.getString(cur.getColumnIndex(dataField));
+        	  if (null != info) {
+        		  infos.add(info);
+        	  }
+        }
+        cur.close();
+		return infos;
+	}
+	
 }
--- Irish Dialer/src/com/prevost/irishdialer/IrishDialerActivity.java
+++ Irish Dialer/src/com/prevost/irishdialer/IrishDialerActivity.java
@@ -3,8 +3,6 @@ package com.prevost.irishdialer;
 import java.util.ArrayList;
 import android.app.Activity;
 import android.os.Bundle;
-import android.content.SharedPreferences;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,17 +13,13 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.database.Cursor;
 import android.provider.ContactsContract;
-import android.provider.ContactsContract.CommonDataKinds.Email;
 import android.provider.ContactsContract.Contacts;
-import android.provider.ContactsContract.Data;
-import android.preference.PreferenceManager;
 
 
 
 public class IrishDialerActivity extends Activity {
 	
 	protected EditText _search = null;
-	protected String[] _projection = null;
 	protected ListView _contacsView = null;
 	protected int _maxContact = 30;
 	protected ArrayList<Contact> _contactList = null;
@@ -89,7 +83,7 @@ public class IrishDialerActivity extends Activity {
     	for (int i = 0 ; (i < n) && (i < _maxContact) ; i++) {
     		String displayName = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
     		int id = cur.getInt(cur.getColumnIndex(ContactsContract.Contacts._ID));
-    		infos = getInfosFromCursorAndPreferences(cur, displayName);
+    		infos = Contact.getInfosFromCursorAndPreferences(getContentResolver(), getBaseContext(), cur, displayName);
     		Contact contact = new Contact(id, displayName, infos);
     		_contactList.add(contact);
     		cur.moveToNext();
@@ -101,104 +95,5 @@ public class IrishDialerActivity extends Activity {
     	cur.close();
     }
     
-    /*
-     ContactsContract.Contacts
-	     display_name
-	     times_contacted
-	     last_time_contacted
-	     _id
-	     photo_id
-	     has_phone_number
-     
-     */
-    
-    /*
-    protected void loadPreferences() {
-    	//SharedPreferences settings = getSharedPreferences(PREFS_NAME, Context.MODE_WORLD_READABLE);
-    	SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-        boolean s = settings.getBoolean("full_name", true);
-        Toast toast = Toast.makeText(getApplicationContext(), "s:" + s, Toast.LENGTH_SHORT);
-        toast.show();
-    }*/
-    
-    protected ArrayList<String> getInfosFromCursorAndPreferences(Cursor cur, String fullName) {
-    	ArrayList<String> infos = new ArrayList<String>();
-    	SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-        
-    	String contactId = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
-    	
-    	if (settings.getBoolean("full_name", true)) {
-        	if (null != fullName) {
-        		infos.add(fullName);
-        	} else {
-        		// TODO: delete this line, added for debugging purpose
-        		// it ensure that every contact have at least one info
-        		// we don't want to match this later
-        		infos.add("(noname)");
-        	}
-        }
-    	
-    	if (settings.getBoolean("email", true)) {
-	    	Cursor emailCur = getContentResolver().query(        
-	    			ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
-	    			ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",  
-	    			new String[]{contactId}, null); 
-	 		while (emailCur.moveToNext()) {   
-	 		    String email = emailCur.getString(emailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
-	 	 	    if (null != email) {
-	 	 	    	infos.add(email);
-	 	 	    }
-	 		} 
-		    emailCur.close();	
-    	}
-    	
-    	if (settings.getBoolean("phone", true)) {
-        	Cursor phoneCur = getContentResolver().query(
-        			ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, 
-         		    ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?", 
-         		    new String[]{contactId}, null);
- 	        while (phoneCur.moveToNext()) {
- 	        	  String phone = phoneCur.getString(phoneCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
- 	        	  if (null != phone) {
- 	        		  infos.add(phone);
- 	        	  }
- 	        } 
- 	        phoneCur.close();
-    	}
-    	
-    	if (settings.getBoolean("nickname", true)) {
-        	Cursor nickCur = getContentResolver().query(
-        			ContactsContract.Data.CONTENT_URI , null, 
-         		    ContactsContract.CommonDataKinds.Nickname.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?", 
-         		    new String[]{contactId, ContactsContract.CommonDataKinds.Nickname.CONTENT_ITEM_TYPE}, null);
- 	        while (nickCur.moveToNext()) {
- 	        	  String nick = nickCur.getString(nickCur.getColumnIndex(ContactsContract.CommonDataKinds.Nickname.NAME));
- 	        	  if (null != nick) {
- 	        		  infos.add(nick);
- 	        	  }
- 	        }
- 	        nickCur.close();
-    	}
-    	
-        return infos;
-    }
-    
-    protected void reloadProjectionFromPreferences() {
-    	_projection = null;
-    	
-    	/*
-    	fullName
-    	email
-    	phone
-    	department
-    	url
-    	instantMessage
-    	jobTitle
-    	nickname
-    	note
-    	organization
-    	*/
-    	
-    }
     
 }
