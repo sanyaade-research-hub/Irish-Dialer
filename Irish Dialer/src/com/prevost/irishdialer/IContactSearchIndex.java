38d66f6c7ab85ad03431f693581ba612384e77c5
Clement PREVOST <prevostclement@gmail.com>
2012-04-26 22:22:40 +0200

    .gitignore clean

--- Irish Dialer/src/com/prevost/irishdialer/IContactSearchIndex.java
+++
@@ -1,9 +0,0 @@
-package com.prevost.irishdialer;
-
-import java.util.List;
-import android.content.SharedPreferences;
-
-public interface IContactSearchIndex {
-	public List<IContact> search(String querystr);	
-	public void addContact(IContact c, SharedPreferences settings);	
-}
